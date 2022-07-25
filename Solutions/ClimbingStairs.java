84fe7f1ccdc83eea6f50210c420febc6bddbf4e7
Vikash Kotteeswaran <71982791+vikash-kotteeswaran@users.noreply.github.com>
2022-07-25 22:57:40 +0530

    Climbing Stairs Problem has been finished

---
+++ Solutions/ClimbingStairs.java
@@ -0,0 +1,58 @@
+class Solution {
+    
+//     Note: Checkout Binets Method
+    
+//     approach 1: Permutations - Not accurate as the calculations in perm return inaccurate answers
+    
+//     public double perm(double x, double y, double z, double memo){
+//         if(x<=1){
+//             return memo;
+//         }
+        
+//         y = y<=1? 1 : y;
+//         z = z<=1? 1 : z;
+        
+//         return perm(x-1, y <= 1? 1 : y-1, z <= 1? 1 : z-1, memo*x/y/z);
+//     }
+    
+//     public int climbStairs(int n) {
+//         double N = n;
+//         int maxTwoSteps = n/2;
+//         int waysForNSteps = 0;
+//         for(int i = 0; i <= maxTwoSteps; i++){
+//             waysForNSteps += (int) perm((N - i*2) + i , i, N - i*2, 1);
+//         }
+        
+//         return waysForNSteps;
+//     }
+    
+    
+//     approach 2: Bottom-Up DP
+    
+    public int climbStairs(int n) {
+        if(n<=2) {
+            return n;
+        }
+        
+        int WaysFor2StairCase = 2;
+        int WaysFor1StairCase = 1;
+        int WaysForNStairCase = 0;
+        
+        for(int i = 2; i < n; i++) {
+            WaysForNStairCase = WaysFor1StairCase + WaysFor2StairCase;
+            int temp = WaysFor2StairCase;
+            WaysFor2StairCase = WaysForNStairCase;
+            WaysFor1StairCase = temp;
+        }
+        
+        return WaysForNStairCase;
+    }
+    
+//     approach 3: Top-Down DP - Time Limit Exceeds due to multiple branching and multiple computations
+    
+//     public int climbStairs(int n){
+//         if(n<=3) return n;
+//         return climbStairs(n - 1) + climbStairs(n - 2);
+//     }
+    
+}
